63a8f4868e018be4dcb9d61089e3b11d911ef42e
Silamoth <michaelbiancardi@icloud.com>
2019-06-06 17:09:18 -0500

    Hill should now work for encryption, but doesn't take into account non-alphabetic characters

--- src/HillPanel.java
+++ src/HillPanel.java
@@ -64,62 +64,104 @@ public class HillPanel extends CipherPanel
 			index += split;
 		}
 		
-		String input = getInput();
+		String input = getInput().toLowerCase();
+		int originalLength = input.length();
 		
-		int matrices = input.length() / keyString.length() + 1;	//TODO: Double check if need to add another or not; I think I do
+		//Pad input to be divisible by key
+		
+		while (input.length() % keyString.length() != 0)
+		{
+			input += "a";
+		}
+		
+		int matrices = input.length() / keyString.length();
 		index = 0;
-		split = input.length() / matrices;
+		split = keyString.length();
 		ArrayList<int[][]> inputMatrices = new ArrayList<int[][]>();
 		
 		for (int i = 0; i < matrices; i++)
 		{
-			String matrixString = input.substring(index, index + split);
-			index += split;
+			String matrixString;
+			try
+			{
+				matrixString = input.substring(index, index + split);
+				index += split;
+			}
+			catch (Exception ex)
+			{
+				matrixString = input.substring(index);
+				index += split;
+			}
 			
 			//Now have letters for a matrix; must now create the matrix
 			
-			int matrixSplit = matrixString.length() / length;
-			int matrixIndex = 0;
-			
 			int newMatrix[][] = new int[length][length];
 			
-			for (int x = 0; x < matrixSplit; x++)
+			//System.out.println(matrixString);	//Confirms that input is being split up properly - at least for inputs divisible by key
+			
+			char[] row = matrixString.toCharArray();
+			int counter = 0;
+			for (int x = 0; x < length; x++)
 			{
-				char[] row = matrixString.substring(matrixIndex, matrixIndex + matrixSplit).toCharArray();
-				matrixIndex += matrixSplit;
-				
-				for (int y = 0; y < row.length; y++)
+				for (int y = 0; y < length; y++)
 				{
-					try
-					{
-						newMatrix[x][y] = (int)row[y] - 97;	//Adjust for ASCII values
-					}
-					catch (Exception ex)
-					{
-						newMatrix[x][y] = 0;
-					}
-				}				
+					newMatrix[y][x] = row[counter] - 97;
+					counter++;
+				}
 			}
 			
-			inputMatrices.add(newMatrix);
+			inputMatrices.add(newMatrix);		//Properly populated
 		}
 		
 		//Now have encryption matrix and ArrayList of input matrices
 		//Can't do this in previous set of loops because matrix multiplication
 		
-		ArrayList<char[][]> outputMatrices = new ArrayList<char[][]>();
+		String output = "";
 		
 		//Loop through each input matrix
 		for (int i = 0; i < inputMatrices.size(); i++)
 		{	
-			char[][] intResult = multiplyMatrices(encryptionMatrix, inputMatrices.get(i));
+			int[][] temp = inputMatrices.get(i);
+			char[][] result = multiplyMatrices(encryptionMatrix, inputMatrices.get(i));
 			
-			
+			for (int x = 0; x < result.length; x++)
+			{
+				for (int y = 0; y < result[x].length; y++)
+				{
+					output += result[y][x];
+				}
+			}
 		}
+		
+		setOutput(output.substring(0, originalLength));
+		//setOutput(output);
 	}
 	
+	//Assume equally-sized square matrices
+	//Safe assumption for this purpose
 	char[][] multiplyMatrices(int[][] matrixOne, int[][] matrixTwo)
 	{
-		return null;
+		char[][] output = new char[matrixOne.length][matrixOne.length];
+		
+		//Loop through each row of first matrix
+		for (int i = 0; i < matrixOne.length; i++)
+		{
+			//Each row of first needs to get paired with each column of second
+			
+			for (int x = 0; x < matrixTwo[0].length; x++)
+			{
+				int sum = 0;
+				
+				for (int y = 0; y < matrixTwo.length; y++)
+				{
+					sum += matrixOne[i][y] * matrixTwo[y][x];
+				}
+				
+				output[i][x] = (char)((sum % 26) + 97);
+				sum = 0;
+			}
+		}
+		
+		return output;
 	}
 }
